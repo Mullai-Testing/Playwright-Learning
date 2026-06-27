61fc399f980fb3a63281e6fcbac1d51d3ac723d2
Mullai-Testing <mullaimalar.tpt@gmail.com>
2026-06-27 14:04:22 +0530

    Add files via upload

---
+++ day1/01-datatype.js
@@ -0,0 +1,58 @@
+/*Code to check for Non-blocking I/O of JavaScript in console devtools of the browser
+-----------------------------------------------------------------------------------
+
+console.log("PlayButton")
+setTimeout(()=>{console.log("Movie Played")},3000)
+console.log("Advertisement")*/
+
+//Primitive Datatypes:
+
+/* Datatypes in JavScript :
+
+1. number
+2. string
+3. boolean
+4. undefined
+5. null */
+
+
+//Declaration
+
+// var phoneNumber // declaration opening a memory location to tstore the number
+
+// //Assignment
+// phoneNumber =687687678
+
+// //Initilization = Declarion + Assignment
+
+// var contactNumber = 797988988
+
+
+//numbers
+
+var phoneNumber =797988988;
+console.log(phoneNumber);
+console.log(typeof phoneNumber );
+
+
+//string
+
+var username = "ravi@testleaf.com" // " ", '' , `` (Backtick=>in keyboard above tab button)
+var password = "@Testlea"
+
+
+console.log(username);
+console.log(typeof username);
+
+
+//boolean
+
+var isJavaScriptIntresting = true
+console.log(isJavaScriptIntresting);
+
+console.log(typeof isJavaScriptIntresting);
+
+//undefined
+
+var accountNumber // only declaration is happening hence undefined 
+console.log(accountNumber);//undefined
